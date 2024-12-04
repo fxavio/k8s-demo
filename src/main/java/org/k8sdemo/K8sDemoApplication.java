ee04a18a2b7ee5dc6df28b3390a78d9b42ca0254
fxavio <xavier.rucahobatinya@amalitech.com>
2024-12-04 16:01:09 +0200

    comment

--- src/main/java/org/k8sdemo/K8sDemoApplication.java
+++ src/main/java/org/k8sdemo/K8sDemoApplication.java
@@ -1,15 +1,19 @@
 package org.k8sdemo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 @RestController
 public class K8sDemoApplication {
+    //    We have a simple REST controller that returns a welcome message when the root URL is accessed.
     @GetMapping
     public String welcomeMessage() {
         return "Welcome to the world of Kubernetes !!";
     }
+
     public static void main(String[] args) {
         SpringApplication.run(K8sDemoApplication.class, args);
     }
